bf513b3dfb4135ba0449ce6504926ce32edd71be
rich0024 <yance0022@gmail.com>
2021-03-17 15:22:16 -0400

    main menu

--- client/src/container/main.js
+++ client/src/container/main.js
@@ -5,16 +5,22 @@ import SignUp from '../components/signup';
 //import SignOut from '../components/signout';
 //import {loginUser} from '../actions/userAction';
 //import {createUser} from '../actions/userAction'
-
+import Menu from '../assets/menu.png'
 
 class Main extends Component {
     render() {
         return (
             <div>
+                <br></br>
+                <br></br>
+                <br></br>
+            <div className="login-menu">
                 <h1>Welcome to RecipeBook!</h1>
+                <img className='menu-img' src={Menu}></img>
                 <Login />
                 <p>or</p>
                 <SignUp />
+            </div>
             </div>
         )
     }
