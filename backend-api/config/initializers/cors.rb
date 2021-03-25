7b21e85864b0090871ca8fb9036092def5e6b41e
rich0024 <yance0022@gmail.com>
2021-03-25 14:36:07 -0400

    samesite

--- backend-api/config/initializers/cors.rb
+++ backend-api/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://warm-island-73271.herokuapp.com'
+    origins ['http://localhost:3001', 'https://warm-island-73271.herokuapp.com']
 
     resource '*',
       headers: :any,
