9669e704bdf19470fe7cefd89e9c9df993cdd454
rich0024 <yance0022@gmail.com>
2021-03-26 11:41:39 -0400

    cors edit

--- backend-api/config/initializers/cors.rb
+++ backend-api/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['http://localhost:3001', 'https://warm-island-73271.herokuapp.com']
+    origins '*'
 
     resource '*',
       headers: :any,
