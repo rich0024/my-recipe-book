a24c076824bf330a62eab3053d295954f1223aaf
rich0024 <yance0022@gmail.com>
2021-03-29 15:37:12 -0400

    heroku

--- backend-api/app/controllers/users_controller.rb
+++ backend-api/app/controllers/users_controller.rb
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
         user = User.new(user_params)
         if user.save
         created_jwt = encode_token({id: user.id})
-        cookies.signed[:jwt] = {value: created_jwt, httponly: true, expires: 1.hour.from_now, SameSite=None}
+        cookies.signed[:jwt] = {value: created_jwt, httponly: true, expires: 1.hour.from_now, same_site: none, secure: true}
         render json: user
         else
             render json: {error: 'Error creating user'}
